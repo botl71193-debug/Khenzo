07fe5cf895ffa973d50a9738f955337dda71bf65
botl71193-debug <botl71193@gmail.com>
2026-06-18 19:01:41 +0700

    Update apks.js

--- apks.js
+++ apks.js
@@ -1,5 +1,15 @@
 const apks = [
     {
+        "name": "BeautyPlus",
+        "version": "V 7.39.0",
+        "category": "Editor",
+        "size": "354,34M",
+        "android": "Android 6.0+",
+        "description": "Editor foto & video AI dengan fitur premium unlocked, autopilot beauty, dan stiker eksklusif tanpa iklan.",
+        "imageUrl": "https://i.supaimg.com/1ac260ce-b26f-4f02-a315-4072e31459d4/1f57c4f2-fca5-4414-9788-e3824fa18c2e.png",
+        "downloadUrl": "https://safefileku.com/download/sRLfffZabhvBsSCl"
+    },
+    {
         "name": "Instagram Pro",
         "version": "V 423.0.0.47.66",
         "category": "Aplikasi Mod",
