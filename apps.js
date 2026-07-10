29db920c24138049168725d153460da20223f887
botl71193-debug <botl71193@gmail.com>
2026-07-10 16:04:49 +0800

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,16 @@
+var myWebApksDatabase = [
+  { 
+    "name": "FreeReals", 
+    "version": "v2.4.0", 
+    "size": "91,32M", 
+    "imageUrl": "https://i.ibb.co.com/...", 
+    "downloadUrl": "https://safefileku.com/..." 
+  },
+  {
+    "name": "BeautyPlus",
+    "version": "V 7.39.0",
+    "size": "173,08M",
+    "imageUrl": "https://i.ibb.co.com/...",
+    "downloadUrl": "https://safefileku.com/..."
+  }
+];
